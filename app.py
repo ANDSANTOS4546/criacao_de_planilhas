e2006d72a56e5725a044d2c84e9566489d473ad4
André Nascimento dos Santos <andre_nascimento4546@hotmail.com>
2021-02-16 14:50:24 -0300

    Adicionando páginas a planilha

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
+from os import truncate
 from openpyxl import Workbook
 
 wb = Workbook()
 
+# Criação de página
+while True:
+    nome_pagina = str(input('Nome da página: ')).strip().title()
+    wb.create_sheet(nome_pagina)
 
-nome_guia = str(input('Nome da guia: '))
+    while True:
+        adicionar_pagina = str(input('Quer adicionar outra página? [S/N]: ')).strip().upper()[0]
+        if "S" != adicionar_pagina != "N":
+            continue
+        else:
+            break
+    if adicionar_pagina == "N":
+        break
 
-wb.create_sheet(nome_guia)
-
+print(wb.sheetnames)
+            
+""" 
 # Salvar arquivo
-wb.save("Teste.xlsx")
+nome_arquivo = str(input("Qual nome deseja salvar seu arquivo: ")).capitalize()
+wb.save(nome_arquivo + ".xlsx")
+ """
