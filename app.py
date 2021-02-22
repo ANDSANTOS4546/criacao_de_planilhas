f882afdc5ca8b2a21ad40a752a5231652bd316e3
André Nascimento dos Santos <andre_nascimento4546@hotmail.com>
2021-02-21 22:01:22 -0300

    Remoçao das funções

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from openpyxl import Workbook
 
+
 def opcao_usario(msg):
     while True:
         opcao = str(input(msg)).strip().upper()[0]
@@ -9,38 +10,26 @@ def opcao_usario(msg):
             return opcao
 
 
-def criar_pagina():
-    while True:
-        nome_pagina = str(input('Nome da página: ')).strip().title()
-        wb.create_sheet(nome_pagina)
-
-        adicionar_pagina = opcao_usario('Quer adicionar outra página? [S/N]: ')
-        if adicionar_pagina == "N":
-            break
-
-
-def selecionar_pagina():
-    escolher_pagina = str(input('Qual página deseja manipular: ')).strip().title()
-    pagina_escolhida = wb[escolher_pagina]
-    return (pagina_escolhida)
-
-
-
 wb = Workbook()
-ws = wb.active    
+ws = wb.active
 
-# Criação de página    
-criar_pagina()
+# Criação de página
+while True:
+    nome_pagina = str(input('Nome da página: ')).strip().title()
+    wb.create_sheet(nome_pagina)
+
+    adicionar_pagina = opcao_usario('Quer adicionar outra página? [S/N]: ')
+    if adicionar_pagina == "N":
+        break
 print(wb.sheetnames)
 
 # Escolher pagina para manipular
-selecionar_pagina()
-
+escolher_pagina = str(input('Qual página deseja manipular: ')).strip().title()
+pagina_escolhida = wb[escolher_pagina]
+print(pagina_escolhida)
 # Insrerir dados na página escolhida
+info = str(input('Teste: '))
 
-
-""" 
 # Salvar arquivo
 nome_arquivo = str(input("Qual nome deseja salvar seu arquivo: ")).capitalize()
 wb.save(nome_arquivo + ".xlsx")
- """
