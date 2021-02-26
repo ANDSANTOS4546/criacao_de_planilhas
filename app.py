5a18981d27680dd15385f3ebfc589f0c0ffb6f34
André Nascimento dos Santos <andre_nascimento4546@hotmail.com>
2021-02-26 17:16:17 -0300

    Criando funçoes

--- app.py
+++ app.py
@@ -10,26 +10,41 @@ def opcao_usario(msg):
             return opcao
 
 
+def criar_paginas():
+    while True:
+        nome_pagina = str(input('Nome da página: ')).strip().title()
+        wb.create_sheet(nome_pagina)
+    
+        adicionar_pagina = opcao_usario('Quer adicionar outra página? [S/N]: ')
+        if adicionar_pagina == "N":
+            break
+
+
+def selecionar_pagina():
+    escolher_pagina = str(input('Selecio página deseja manipular: ')).strip().title()
+    pagina_escolhida = wb[escolher_pagina]
+    return pagina_escolhida
+
+
+def salvar_arquivo():
+    nome_arquivo = str(input("Qual nome deseja salvar seu arquivo: ")).capitalize()
+    wb.save(nome_arquivo + ".xlsx")
+
+
 wb = Workbook()
 ws = wb.active
 
 # Criação de página
-while True:
-    nome_pagina = str(input('Nome da página: ')).strip().title()
-    wb.create_sheet(nome_pagina)
+criar_paginas()
 
-    adicionar_pagina = opcao_usario('Quer adicionar outra página? [S/N]: ')
-    if adicionar_pagina == "N":
-        break
+del wb['Sheet']  # Deletar a pagina "Sheet"
 print(wb.sheetnames)
 
 # Escolher pagina para manipular
-escolher_pagina = str(input('Qual página deseja manipular: ')).strip().title()
-pagina_escolhida = wb[escolher_pagina]
-print(pagina_escolhida)
+selecionar_pagina()
+
 # Insrerir dados na página escolhida
-info = str(input('Teste: '))
+
 
 # Salvar arquivo
-nome_arquivo = str(input("Qual nome deseja salvar seu arquivo: ")).capitalize()
-wb.save(nome_arquivo + ".xlsx")
+salvar_arquivo()
